dab2fb8d001766d107263956eefc11999ec216b4
farrukhrakhimoff <farrukhrakhimoff@gmail.com>
2022-10-13 17:11:02 +0500

    new commit

--- Scripts/objects.js
+++ Scripts/objects.js
@@ -1,19 +1,13 @@
 'Use strict'
 
-const thief = {
-    jacket: 'Blue',
-    height: 1.7,
-    details: {
-        hair: 'jet black',
-        style: 'long'
-    }
-}
-
-let person = {
-    age: 3,
-    name: 'Farrux',
-    ismarried: true
-}
+// const thief = {
+//     jacket: 'Blue',
+//     height: 1.7,
+//     details: {
+//         hair: 'jet black',
+//         style: 'long'
+//     },
+// }
 
 // console.log(thief.height);
 
@@ -28,6 +22,49 @@ let person = {
 // ----------
 // FOR IN //
 // ----------
-for(let key in thief) { // for in ni object ishlatish orqali uning property lariga yoki uning value lariga murojat qilish mumkin
-    console.log(`Property ${key} has a value of ${thief[key]}`)
-}
+// let count = 0;
+// for(let key in thief) { // for in ni object ishlatish orqali uning property lariga yoki uning value lariga murojat qilish mumkin
+//     count++;
+//     /*if(typeof thief[key] === "object"){
+//         for(let i in thief[key]){ //agar object ichidagi objectni value si ni value sini chiqarmoqchi bo'lsangiz, here we go
+//             console.log(`Property ${i} has a value: ${thief[key][i]}`)
+//         }
+//     }
+//     else{
+//         console.log(`Property ${key} has a value: ${thief[key]}`)            
+//         };*/
+// }
+// console.log(count);
+// -------------------------
+// The First Method has been written by you!
+// let person = {
+    //     age: 3,
+    //     name: 'Farrux',
+    //     ismarried: true,
+    //     howOut: function(){ //method bu object ichida e value sifatida e'lon qilingan qiymatidir
+    //         console.log("that's genious!");
+    //     }
+    // }
+    // person.howOut(); //method ni shunday chaqiriladi!
+    // -------------------------
+
+    // Distrubtizatsiya
+    // -------------------------
+
+    const thief = {
+        jacket: 'Blue',
+        height: 1.7,
+        details: {
+            hair: 'jet black',
+            style: 'long',
+            color: 'black'
+        },
+    }
+
+    const {hair, style, color} = thief.details; //shu usul bilan object ichidagi objectning value larini olish mumkin boladi
+
+    console.log(hair);
+    console.log(style);
+    console.log(color);
+    
+
